9ca1c8a769212080a8a203f6b626483d30723c27
Sagarmoy Sengupta <sagarmoysengupta@gmail.com>
2021-05-04 09:29:34 +0530

    Checking

--- pages/index.js
+++ pages/index.js
@@ -32,38 +32,39 @@ export default function Home() {
         Hello Covid
       </h1>
       {
-        data.length > 0 ?
+        check ? 'Checking' :
+          data.length > 0 ?
+            <h5>
+              {
+                data?.map((element, index) => {
+                  const isAudioRequired = element.available_capacity > 0;
+                  return (
+                    <div key={index} style={{
+                      margin: '15px',
+                      border: '1px solid #cecece',
+                      padding: '15px'
+                    }}>
+                      <h3>
+                        {element.centerName}
+                      </h3>
+                      <h4>
+                        {element.available_capacity}
+                      </h4>
+                      {
+                        isAudioRequired ? <ReactAudioPlayer src="cov.mp3"
+                          autoPlay={true}
+                          controls
+                          loop={element.available_capacity > 0}
+                        >
+                        </ReactAudioPlayer> : ""
+                      }
 
-          <h5>
-            {data?.map((element, index) => {
-              const isAudioRequired = element.available_capacity > 0;
-              return (
-                <div key={index} style={{
-                  margin: '15px',
-                  border: '1px solid #cecece',
-                  padding: '15px'
-                }}>
-                  <h3>
-                    {element.centerName}
-                  </h3>
-                  <h4>
-                    {element.available_capacity}
-                  </h4>
-                  {
-                    isAudioRequired ? <ReactAudioPlayer src="cov.mp3"
-                      autoPlay={true}
-                      controls
-                      loop={element.available_capacity > 0}
-                    >
-                    </ReactAudioPlayer> : ""
-                  }
+                    </div>)
+                }
 
-                </div>)
-            }
-
-            )}
-          </h5>
-          : <h1>No Available Slots</h1>}
+                )}
+            </h5>
+            : <h1>{'No Available Slots'}</h1>}
     </>
   )
 }
